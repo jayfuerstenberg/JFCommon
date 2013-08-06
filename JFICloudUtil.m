9ac7bc06aeefb4cb5f17a8b94a52f65036203515
Jason Fuerstenberg <jay@jayfuerstenberg.com>
2013-08-06 23:52:29 +0900

    Adding iCloud util class.
    Simple util class for preventing the automatic upload/backup of files
    to iCloud in iOS 5.0 and later.

---
+++ JFICloudUtil.m
@@ -0,0 +1,34 @@
+//
+//  JFICloudUtil.m
+//  JFCommon
+//
+//  Created by Jason Fuerstenberg on 13/1/28.
+//  Copyright (c) 2013 Jason Fuerstenberg. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import "JFICloudUtil.h"
+
+@implementation JFICloudUtil
+
++ (BOOL) addSkipBackupAttributeToItemAtURL: (NSString *) url {
+	
+    const char *filePath = [url UTF8String];
+    const char *attrName = "com.apple.MobileBackup";
+    u_int8_t attrValue = 1;
+	
+    int result = setxattr(filePath, attrName, &attrValue, sizeof(attrValue), 0, 0);
+    return result == 0;
+}
+
+@end
