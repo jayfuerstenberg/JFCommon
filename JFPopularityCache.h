1a86a3ce8d6748c2518283f789de3e7647ad8095
Jason Fuerstenberg <jay@jayfuerstenberg.com>
2014-05-12 17:02:59 -0700

    Adding my LRU cache code.
    This class provides an LRU cache for storing objects in a
    memory-restrained environment.

---
+++ JFPopularityCache.h
@@ -0,0 +1,53 @@
+//
+//  JFPopularityCache.h
+//  JFCommon
+//
+//  Created by Jason Fuerstenberg on 2010/04/26.
+//  Copyright 2010 Jason Fuerstenberg. All rights reserved.
+//
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
+
+#import <Foundation/Foundation.h>
+
+
+// The default max capacity
+#define JFPopularityCacheDefaultMaxCapacity		10
+
+/*
+ * The popularity cache.
+ * Objects fall out of the cache from the rear when the object count exceeds the maximum capacity.
+ * Objects that are just added are considered popular and are pushed to the front of the cache.
+ */
+@interface JFPopularityCache : NSObject {
+	
+	// The cache's maximum capacity.
+	NSUInteger _maxCapacity;
+	
+	// The inner cache dictionary.
+	NSMutableDictionary *_cache;
+	
+	// The popularity rank.
+	NSMutableArray *_popularity;
+}
+
+- (void) setMaxCapacity: (NSUInteger) maxCapacity;
+- (NSUInteger) maxCapacity;
+- (NSUInteger) objectCount;
+- (void) clear;
+- (id) addObject: (id) object withKey: (NSString *) key;
+- (id) removeObjectWithKey: (NSString *) key;
+- (id) removeLastObject;
+- (BOOL) containsObject: (id) object;
+- (id) objectWithKey: (NSString *) key;
+
+@end
---
+++ JFPopularityCache.m
@@ -0,0 +1,221 @@
+//
+//  JFPopularityCache.m
+//  JFCommon
+//
+//  Created by Jason Fuerstenberg on 2010/04/26.
+//  Copyright 2010 Jason Fuerstenberg. All rights reserved.
+//
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
+
+#import "JFPopularityCache.h"
+
+
+@implementation JFPopularityCache
+
+/*
+ * Initializes the cache.
+ *
+ * Return
+ *		The instance.
+ */
+- (id) init {
+	
+	self = [super init];
+	
+	_cache = [[NSMutableDictionary alloc] initWithCapacity: JFPopularityCacheDefaultMaxCapacity];
+	_popularity = [[NSMutableArray alloc] initWithCapacity: JFPopularityCacheDefaultMaxCapacity];
+	_maxCapacity = JFPopularityCacheDefaultMaxCapacity;
+	
+	return self;
+}
+
+#if  __has_feature(objc_arc)
+
+#else
+
+- (void) dealloc {
+	
+	[_cache release];
+	[_popularity release];
+	[super dealloc];
+}
+
+#endif
+
+- (void) setMaxCapacity: (NSUInteger) maxCapacity {
+	
+	if (maxCapacity < 1) {
+		return;
+	}
+	
+	NSUInteger objectCount = [self objectCount];
+	if (maxCapacity < objectCount) {
+		/*
+		 * The max capacity just shrunk below what the current object count.
+		 * Kick out the excessive objects.
+		 */
+		for (NSUInteger loop = 0; loop < (objectCount - maxCapacity); loop++) {
+			[self removeLastObject];
+		}
+	}
+	
+	_maxCapacity = maxCapacity;
+}
+
+- (NSUInteger) maxCapacity {
+	
+	return _maxCapacity;
+}
+
+- (NSUInteger) objectCount {
+	
+	NSUInteger count;
+	@synchronized (_cache) {
+		count = [_cache count];
+	}
+	
+	return count;
+}
+
+- (void) clear {
+	
+	@synchronized (_cache) {
+		@synchronized (_popularity) {
+			[_cache removeAllObjects];
+			[_popularity removeAllObjects];
+		}
+	}
+}
+
+- (id) addObject: (id) object withKey: (NSString *) key {
+	
+	if (object == nil) {
+		return nil;
+	}
+	
+	if ([key length] == 0) {
+		return nil;
+	}
+	
+	if (![self containsObject: object]) {
+		// The object does not exist in the cache so add it...
+		@synchronized (_cache) {
+			@synchronized (_popularity) {
+				[_cache setObject: object
+						   forKey: key];
+				[_popularity insertObject: object
+								  atIndex: 0];
+			}
+		}
+	} else {
+		// The object is in the cache so just reorder the popularity.
+		@synchronized (_popularity) {
+			NSUInteger index = [_popularity indexOfObject: object];
+            if (index != INT_MAX) {
+                [_popularity removeObjectAtIndex: index];
+            }
+			
+			[_popularity insertObject: object
+							  atIndex: 0];
+		}
+	}
+	
+	NSUInteger objectCount = [self objectCount];
+	if (objectCount > _maxCapacity) {
+		// There is one too many objects in the cache so remove the last one.
+		[self removeLastObject];
+	}
+	
+	return object;
+}
+
+- (id) removeObjectWithKey: (NSString *) key {
+	
+	if ([key length] == 0) {
+		return nil;
+	}
+	
+	id object;
+	
+	// Return the element if it exists.
+	@synchronized (_cache) {
+		object = [_cache objectForKey: key];
+	}
+	
+	if (object == nil) {
+		return nil;
+	}
+	
+	// The object will be removed now.
+	@synchronized (_popularity) {
+		NSUInteger index = [_popularity indexOfObject: object];
+		[_popularity removeObjectAtIndex: index];
+		NSArray *allKeys = [_cache allKeysForObject: object];
+		[_cache removeObjectsForKeys: allKeys];
+	}
+	
+	return object;	
+}
+
+- (id) removeLastObject {
+	
+	if ([self objectCount] == 0) {
+		return nil;
+	}
+	
+	id object = nil;
+	@synchronized (_popularity) {
+		@synchronized (_cache) {
+			object = [_popularity lastObject];
+			NSArray *allKeys = [_cache allKeysForObject: object];
+			[_popularity removeLastObject];
+			[_cache removeObjectsForKeys: allKeys];
+		}
+	}
+	
+	return object;
+}
+
+- (BOOL) containsObject: (id) object {
+	
+	if (object == nil) {
+		return NO;
+	}
+	
+	BOOL contained;
+	
+	// Delegate the call to the popularity array.
+	@synchronized (_popularity) {
+		contained = [_popularity containsObject: object];
+	}
+	
+	return contained;
+}
+
+- (id) objectWithKey: (NSString *) key {
+	
+	id object;
+	
+	// Return the element if it exists.
+	@synchronized (_cache) {
+		object = [_cache objectForKey: key];
+	}
+	
+	if (object == nil) {
+		return nil;
+	}
+	
+	return object;
+}
+
+@end
